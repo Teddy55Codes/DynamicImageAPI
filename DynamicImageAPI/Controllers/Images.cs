f199c0d301f6adf7a13426e5c70825ad4d180680
Teddy <teddy@teddy.email>
2023-01-14 20:20:16 +0100

    add Location endpoint. It returns an image of your current city

--- DynamicImageAPI/Controllers/Images.cs
+++ DynamicImageAPI/Controllers/Images.cs
@@ -1,5 +1,9 @@
+using System.Net;
+using System.Text.Json.Nodes;
+using DynamicImageAPI.Models;
 using SkiaSharp;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace DynamicImageAPI.Controllers;
 
@@ -7,6 +11,8 @@ namespace DynamicImageAPI.Controllers;
 [Route("[controller]")]
 public class Images : ControllerBase
 {
+    private string _locationFallback = "Earth";
+    
     [HttpGet("Counter")]
     public IActionResult Get(string counterId = "0", int fontSize = 12, string textColorHEX = "000000")
     {
@@ -30,6 +36,44 @@ public class Images : ControllerBase
         var skData = bitmap.Encode(SKEncodedImageFormat.Png, 0);
 
         return File(skData.ToArray(), "image/png");
+    }
+    
+    [HttpGet("Location")]
+    public IActionResult GetLocation(int fontSize = 12, string textColorHEX = "000000")
+    {
+        string cityLocation = GetCityFromIp(Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty);
+        
+        SKRect bounds = new SKRect();
+        
+        var textPaint = new SKPaint { TextSize = fontSize, Color = SKColor.Parse("#"+textColorHEX) };
+        textPaint.MeasureText(cityLocation, ref bounds);
+        
+        var bitmap = new SKBitmap(
+            (int)bounds.Right+2, 
+            (int)bounds.Height);
 
+        SKCanvas bitmapCanvas = new SKCanvas(bitmap);
+
+        bitmapCanvas.Clear();
+        bitmapCanvas.DrawText(cityLocation, 0, -bounds.Top, textPaint);
+
+        var skData = bitmap.Encode(SKEncodedImageFormat.Png, 0);
+
+        return File(skData.ToArray(), "image/png");
+    }
+
+    private string GetCityFromIp(string ipAddress)
+    {
+        HttpClient client = new HttpClient();
+        
+        client.DefaultRequestHeaders.Add("User-Agent", "DynamicImageAPI");
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+        if (string.IsNullOrEmpty(ipAddress)) return _locationFallback;
+        var response = client.GetAsync($"http://ip-api.com/json/{ipAddress}").Result;
+        if (!response.IsSuccessStatusCode) return _locationFallback;
+        IpAPIRsoponse? ipApiRsoponse = JObject.Parse(response.Content.ReadAsStringAsync().Result).ToObject<IpAPIRsoponse>();
+        if (ipApiRsoponse?.City == null) return _locationFallback;
+        return ipApiRsoponse.City;
     }
 }
