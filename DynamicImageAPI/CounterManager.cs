b76dda99ec7064bdc5fa3701740e58dd6bba7b15
Teddy <teddy@teddy.email>
2023-01-14 19:40:00 +0100

    add CounterManager to save current counter state will later use an sqlite db

---
+++ DynamicImageAPI/CounterManager.cs
@@ -0,0 +1,20 @@
+﻿namespace DynamicImageAPI;
+
+public static class CounterManager
+{
+    private static Dictionary<string, long> _savedCounters = new Dictionary<string, long>() {{"0", 1111110}, {"1", 9999998}, {"2", 111111111111110}};
+
+    public static long GetNewCounterValue(string counterId)
+    {
+        if (_savedCounters.ContainsKey(counterId))
+        {
+            _savedCounters[counterId]++;
+        }
+        else
+        {
+            _savedCounters[counterId] = 1;
+        }
+        
+        return _savedCounters[counterId];
+    }
+}
