9e50f45ecce7e4c659ee9caf7cc4848759488bff
Teddy <teddy@teddy.email>
2023-01-15 09:51:20 +0100

    CounterManager now using sqlite db

--- DynamicImageAPI/CounterManager.cs
+++ DynamicImageAPI/CounterManager.cs
@@ -1,20 +1,24 @@
-﻿namespace DynamicImageAPI;
+﻿using System.Reflection;
 
-public static class CounterManager
+namespace DynamicImageAPI;
+
+public static class DataManager
 {
-    private static Dictionary<string, long> _savedCounters = new Dictionary<string, long>();
-
     public static long GetNewCounterValue(string counterId)
     {
-        if (_savedCounters.ContainsKey(counterId))
+        var counterDb = SQLiteDBAccess.SQLiteDBAccess.Instance("Images", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+        if (!counterDb.CheckForExistingElementByAttribute("Counters", "Name", counterId))
         {
-            _savedCounters[counterId]++;
+            counterDb.Insert("Counters", "Name, Count", $"'{counterId}', 0");
         }
-        else
-        {
-            _savedCounters[counterId] = 1;
-        }
-        
-        return _savedCounters[counterId];
+        var reader = counterDb.GetByAttribute("Counters", "Name", counterId);
+        if (!reader.Read()) return 0;
+        long? counterPrimaryKey = reader.GetInt64(0);
+        long? counterValue = reader.GetInt64(2);
+        counterDb.CloseDBFile(reader);
+        counterValue++;
+        counterDb.UpdateSingle("Counters", "Id", counterPrimaryKey.ToString(), "Count", counterValue.ToString());
+        return counterValue ?? 0;
     }
 }
