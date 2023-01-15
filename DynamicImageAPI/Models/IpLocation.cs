a26d0ac05d6a564f0cc040add915e8d7b101b140
Teddy <teddy@teddy.email>
2023-01-15 09:48:08 +0100

    simplified model to save location

---
+++ DynamicImageAPI/Models/IpLocation.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicImageAPI.Models;
+
+public class IpLocation
+{
+    public string Query { get; set; }
+    public string Status { get; set; }
+    public string City { get; set; }
+    public bool Mobile { get; set; }
+    public bool Proxy { get; set; }
+}
